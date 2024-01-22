234f195c444e2a52eb647d49a816305edb3369c8
TheHoltz <holtzs.william@gmail.com>
2024-01-22 16:26:30 -0300

    feat: :sparkles: added install app top bar

---
+++ components/fixed-top-bar.tsx
@@ -0,0 +1,71 @@
+import React, { MouseEvent, useEffect, useState } from "react";
+
+import { X } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: Array<string>;
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+
+const InstallPWA: React.FC = () => {
+  const [supportsPWA, setSupportsPWA] = useState<boolean>(false);
+  const [promptInstall, setPromptInstall] =
+    useState<BeforeInstallPromptEvent | null>(null);
+  const [close, setClose] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handler = (e: Event) => {
+      e.preventDefault();
+      const beforeInstallPromptEvent = e as BeforeInstallPromptEvent;
+      setSupportsPWA(true);
+      setPromptInstall(beforeInstallPromptEvent);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("beforeinstallprompt", handler);
+  }, []);
+
+  const onClick = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+
+  return (
+    <div
+      className={cn(
+        "fixed top-0 inset-x-0 py-3 bg-white/40 z-50 backdrop-blur-md flex items-center justify-center",
+        {
+          hidden: close,
+        }
+      )}
+    >
+      {!supportsPWA ? (
+        <p className="p-4 text-center text-sm">
+          Add our app to your home screen. Click on the share button and then
+          click on 'Add to Home Screen'
+        </p>
+      ) : (
+        <button onClick={onClick} aria-label="Install app" title="Install App">
+          Click here to add our app to your home screen
+        </button>
+      )}
+
+      <button
+        className="absolute top-0 right-0 p-3"
+        onClick={() => setClose(true)}
+      >
+        <X />
+      </button>
+    </div>
+  );
+};
+
+export default InstallPWA;
--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,19 +1,21 @@
 import { CategoryProvider } from "@/components/category-context";
 import FeedComponent from "../components/feed";
+import FixedTopBar from "@/components/fixed-top-bar";
 import Nav from "../components/navbar";
 import React from "react";
 
 const Feed = () => {
-	return (
-		<CategoryProvider>
-			<div className="relative">
-				<div className="max-w-lg mx-auto">
-					<FeedComponent />
-				</div>
-				<Nav />
-			</div>
-		</CategoryProvider>
-	);
+  return (
+    <CategoryProvider>
+      <div className="relative">
+        <FixedTopBar />
+        <div className="max-w-lg mx-auto">
+          <FeedComponent />
+        </div>
+        <Nav />
+      </div>
+    </CategoryProvider>
+  );
 };
 
 export default Feed;
