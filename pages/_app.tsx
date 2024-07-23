daa557815dbeac53209e9ed6f10fb70f4e534efc
Jainil Sutaria <jainil@privy.io>
2024-07-23 13:23:14 -0400

    pt 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { PrivyProvider } from "@privy-io/react-auth";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
   return (
     <>
       <Head>
