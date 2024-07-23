cf062ee2e4377dbcb3921c9c9285603bb7fd5ebf
Jainil Sutaria <jainil@privy.io>
2024-07-23 13:06:10 -0400

    Remove deprecated onSuccess

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,39 @@
-import '../styles/globals.css';
-import type {AppProps} from 'next/app';
-import Head from 'next/head';
-import {PrivyProvider} from '@privy-io/react-auth';
-import {useRouter} from 'next/router';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { useRouter } from "next/router";
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <>
       <Head>
-        <link rel="preload" href="/fonts/AdelleSans-Regular.woff" as="font" crossOrigin="" />
-        <link rel="preload" href="/fonts/AdelleSans-Regular.woff2" as="font" crossOrigin="" />
-        <link rel="preload" href="/fonts/AdelleSans-Semibold.woff" as="font" crossOrigin="" />
-        <link rel="preload" href="/fonts/AdelleSans-Semibold.woff2" as="font" crossOrigin="" />
+        <link
+          rel="preload"
+          href="/fonts/AdelleSans-Regular.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/AdelleSans-Regular.woff2"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/AdelleSans-Semibold.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/AdelleSans-Semibold.woff2"
+          as="font"
+          crossOrigin=""
+        />
 
         <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
@@ -23,10 +43,7 @@ function MyApp({Component, pageProps}: AppProps) {
         <title>Privy Auth Starter</title>
         <meta name="description" content="Privy Auth Starter" />
       </Head>
-      <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
-        onSuccess={() => router.push('/dashboard')}
-      >
+      <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}>
         <Component {...pageProps} />
       </PrivyProvider>
     </>
