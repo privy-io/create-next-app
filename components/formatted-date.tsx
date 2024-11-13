267422bb399638eb9b33ef486e2eb282cb1ec462
Coby Goldstein <coby@privy.io>
2024-11-13 15:43:59 -0500

    update Next version and format code

--- components/formatted-date.tsx
+++ components/formatted-date.tsx
@@ -2,13 +2,16 @@ type Props = {
   secsSinceEpoch: number;
 };
 
-export default function FormattedDate({secsSinceEpoch}: Props) {
-  const formattedDate = new Date(secsSinceEpoch * 1000).toLocaleDateString('en-us', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
+export default function FormattedDate({ secsSinceEpoch }: Props) {
+  const formattedDate = new Date(secsSinceEpoch * 1000).toLocaleDateString(
+    "en-us",
+    {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    },
+  );
 
   return <p>{formattedDate}</p>;
 }
