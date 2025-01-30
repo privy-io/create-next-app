695f97496bdbfe53f4c8e87f32825d5ea4c19120
dylanprivy <dylan.steck@privy.io>
2025-01-30 19:32:20 +0000

    fix: only look to verify token on `page.tsx` if we have a `cookieAuthToken`

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,22 @@ export default async function Page() {
   const cookieStore = await cookies()
   const cookieAuthToken = cookieStore.get("privy-token")?.value;
 
-  // If no cookie is found, skip any further checks
-  if (!cookieAuthToken) return { props: {} };
+  if (cookieAuthToken){
+    const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
+    const PRIVY_APP_SECRET = process.env.PRIVY_APP_SECRET;
+    const client = new PrivyClient(PRIVY_APP_ID!, PRIVY_APP_SECRET!);
 
-  const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
-  const PRIVY_APP_SECRET = process.env.PRIVY_APP_SECRET;
-  const client = new PrivyClient(PRIVY_APP_ID!, PRIVY_APP_SECRET!);
+    try {
+      const claims = await client.verifyAuthToken(cookieAuthToken);
+      // Use this result to pass props to a page for server rendering or to drive redirects!
+      // ref https://nextjs.org/docs/pages/api-reference/functions/get-server-side-props
+      console.log({ claims });
 
-  try {
-    const claims = await client.verifyAuthToken(cookieAuthToken);
-    // Use this result to pass props to a page for server rendering or to drive redirects!
-    // ref https://nextjs.org/docs/pages/api-reference/functions/get-server-side-props
-    console.log({ claims });
+      redirect("/dashboard");
 
-    redirect("/dashboard");
-
-  } catch (error) {
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return <Login />
